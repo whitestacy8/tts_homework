6185b68c0ea2d68c8864c9845075e33275e9bdc6
stacy white <whitestacy8@gmail.com>
2017-04-17 18:38:14 -0400

    Adding homework22.rb

---
+++ homework22.rb
@@ -0,0 +1,36 @@
+# scores = [100, 85, 30, 79]
+# counter = 0
+# sum = 0
+#
+# while counter < scores.length
+#     sum = sum + scores[counter]
+#     counter += 1 # same as counter = counter + 1
+# end
+#
+# puts "The total is #{sum}"
+
+# "Students & Teachers" - write a program that has two arrays,
+# one of student names and one of teacher names. Ask the user for
+# their name, and greet them according to their role
+# (if they're a teacher, it says one thing,
+# if they're a student, it says something else)
+
+# teachers = ["Scott", "Linda","Olivia"]
+#
+# students = ["Ricky", "Ronnie", "Ralph"]
+#
+# puts "What is your name?"
+# name = gets.chomp.capitalize
+#
+#
+# if name == teachers[0] || name == teachers[1] || name == teachers[2]
+#   puts "Welcome to your class Mr/Mrs #{name}"
+# else
+#   puts "Please take your seat #{name}"
+# end
+
+# Create a program that will roll a dice twice and output the numbers.
+
+#for x in 1..2
+#     puts rand(7)
+# end
